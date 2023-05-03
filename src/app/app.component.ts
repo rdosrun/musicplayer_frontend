695d50984d2a08f1166dff2bbbf98dee903ea35a
Richard <rdosrun@gmail.com>
2023-05-03 10:51:00 -0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SearchListComponent } from './search-list/search-list.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'audio_gui';
+  title = "audio_gui";
+
+  show_control = false;
+  show_results = false;
+  loading = false;
+
+  query = "";
+  result_list:any;
+  song_data:any;
+
+  //audio control move
+  play_status = false;
+  song_title = "";
+  play_time = 0;
+  play_time_str = "0:00";
+
+  //implement way to read config
+  server_addr = "192.168.1.100";
+  port = ":5001";
+
+
+
+
+  async search(query:string){
+    this.loading = true;
+    let r = await fetch("http://"+this.server_addr+this.port+"/search?song="+query)
+      .then(
+        (response:any)=>{
+          return response.json().then(
+            (data:any)=>{
+              console.log(data);
+              this.result_list = data;
+              return data;
+            }
+          ).then(()=>{
+              this.show_results = true;
+              this.loading = false;
+            }
+          )
+        }
+      );
+  }
+
+  async stream(id:number){
+    this.loading = true;
+    console.log(this.result_list[id].videoId);
+    this.song_data = ("http://"+this.server_addr+this.port+"/stream?id="+this.result_list[id].videoId);
+    this.play_status = true;
+    this.show_control = true;
+    this.loading = false;
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AudioControlComponent } from './audio-control/audio-control.component';
+import { SearchListComponent } from './search-list/search-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AudioControlComponent,
+    SearchListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
